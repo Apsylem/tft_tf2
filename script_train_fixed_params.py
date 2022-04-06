252d2ac396b5b736998f8a4f7201f881421bfd12
Apsylem <Alexander.Vocaet@gmail.com>
2022-04-06 11:11:38 +0200

    like hyperparam script and time measurment

--- script_train_fixed_params.py
+++ script_train_fixed_params.py
@@ -67,25 +67,24 @@ def main(expt_name,
         only -- switch to False to use original default settings
     """
     # %%
-    name = 'kidfail'
-    output_folder = '/home/alexander/time_fusion_transformer/tft_outputs'
+    #expt_name = 'kidfail'
+    #output_folder = '/home/alexander/time_fusion_transformer/tft_outputs'
     #output_folder = '/app/tft_outputs'
-    config = ExperimentConfig(name, output_folder)
-    formatter = config.make_data_formatter()
+    #config = ExperimentConfig(expt_name, output_folder)
+    #formatter = config.make_data_formatter()
 
     
-    expt_name=name
+   
     #use_gpu=False
-    use_gpu=True
-    model_folder=os.path.join(config.model_folder, "fixed_klein1")
+    #use_gpu=True
+    #model_folder=os.path.join(config.model_folder, "fixed_klein1")
     #model_folder=os.path.join(config.model_folder, "fixed")
-    data_csv_path=config.data_csv_path
-    data_formatter=formatter
-    use_testing_mode=False
+    #data_csv_path=config.data_csv_path
+    #data_formatter=formatter
+    #use_testing_mode=False
     
     # %%
-    data_formatter = data_formatter(klein=True)
-    #data_formatter = data_formatter(klein=False)
+    
     # %%
     num_repeats = 1
 
@@ -108,12 +107,10 @@ def main(expt_name,
     print("Loading & splitting data...")
     if not data_formatter.pre_split_data_available:
         raw_data = pd.read_csv(data_csv_path, index_col=0)
-        
         train, valid, test = data_formatter.split_data(raw_data)
+        
     if data_formatter.pre_split_data_available:
         train, valid, test = data_formatter.load_splitted_data()
- 
-    
     
     train_samples, valid_samples = data_formatter.get_num_samples_for_calibration()
     
@@ -150,6 +147,7 @@ def main(expt_name,
             tf.keras.backend.set_session(sess)
             
             params = opt_manager.get_next_parameters()
+            
             #from util.general_util import dev_pickle
             #dev_pickle((params),"params")
             #(params) = dev_pickle(False,"params")
@@ -160,7 +158,11 @@ def main(expt_name,
                 model.cache_batched_data(valid, "valid", num_samples=valid_samples)
 
             sess.run(tf.global_variables_initializer())
+            import time
+            before_fit = time.time()
             model.fit()
+            time_to_fit = time.time()-before_fit
+            
 
             val_loss = model.evaluate()
 
@@ -215,59 +217,110 @@ def main(expt_name,
     print()
     print("Normalised Quantile Loss for Test Data: P50={}, P90={}".format(
         p50_loss.mean(), p90_loss.mean()))
-
+    
+    print(f"took {time_to_fit} seconds for {params['epochs']} epochs to fit with gpu {use_gpu}")
 
 if __name__ == "__main__":
     def get_args():
         """Gets settings from command line."""
 
         experiment_names = ExperimentConfig.default_experiments
-
         parser = argparse.ArgumentParser(description="Data download configs")
         parser.add_argument(
-            "expt_name",
-            metavar="e",
+            "-expt_name",  
             type=str,
             nargs="?",
             default="volatility",
             choices=experiment_names,
             help="Experiment Name. Default={}".format(",".join(experiment_names)))
         parser.add_argument(
-            "output_folder",
-            metavar="f",
+            "-output_folder",  
             type=str,
             nargs="?",
             default=".",
             help="Path to folder for data download")
         parser.add_argument(
-            "use_gpu",
-            metavar="g",
+            "-use_gpu",  
             type=str,
             nargs="?",
             choices=["yes", "no"],
             default="no",
             help="Whether to use gpu for training.")
-
+        parser.add_argument(
+            "-use_testing_mode",  
+            type=str,
+            nargs="?",
+            choices=["yes", "no"],
+            default="yes",
+            help="Whether to use testing.")
+        parser.add_argument(
+                "-klein",      
+                type=str,
+                nargs="?",
+                choices=["yes", "no"],
+                default="no",
+                help="wether to use klein Dataset")
+        parser.add_argument(
+                "-num_encoder_steps",      
+                type=int,
+                nargs='?',
+                default=56,
+                help="how many intervalls should be given es as input")
+        parser.add_argument(
+                "-n_timesteps_forecasting",      
+                type=int,
+                nargs='?',
+                default=20,
+                help="how many intervalls should be forecasted")
+        parser.add_argument(
+                "-timeseries_interval",      
+                type=int,
+                nargs='?',
+                default=6,
+                help="how long one timeseries intervall should be")
+        parser.add_argument(
+                "-input_t_dim",      
+                type=int,
+                nargs='?',
+                default=120,
+                help="how long one timeseries intervall should be")
+        parser.add_argument(
+                "-num_epochs",      
+                type=int,
+                nargs='?',
+                default=200,
+                help="how many epochs to train")
+   
         args = parser.parse_known_args()[0]
 
         root_folder = None if args.output_folder == "." else args.output_folder
-        return args.expt_name, root_folder, args.use_gpu == "yes"
-
-    # %%
-    name, output_folder, use_tensorflow_with_gpu = get_args()
-
-    print("Using output folder {}".format(output_folder))
-
-    config = ExperimentConfig(name, output_folder)
-    formatter = config.make_data_formatter()
     
+        return args.expt_name, root_folder, args.use_gpu == 'yes', \
+            args.use_testing_mode == 'yes', args.klein == 'yes',\
+            args.num_encoder_steps,args.n_timesteps_forecasting,args.timeseries_interval,args.input_t_dim,args.num_epochs
+
+    # Load settings for default experiments
+    name, folder, use_tensorflow_with_gpu, use_testing_mode, klein, num_encoder_steps,n_timesteps_forecasting,timeseries_interval,input_t_dim,num_epochs = get_args()
+
+    print("Using output folder {}".format(folder))
+
+    config = ExperimentConfig(name, folder)
+    formatter = config.make_data_formatter()
+    formatter = formatter(klein=klein,
+                        num_encoder_steps = num_encoder_steps ,
+                        n_timesteps_forecasting = n_timesteps_forecasting,
+                        timeseries_interval = timeseries_interval,
+                        input_t_dim = input_t_dim,
+                        num_epochs = num_epochs)
     # Customise inputs to main() for new datasets.
     
     main(
         expt_name=name,
         use_gpu=use_tensorflow_with_gpu,
-        model_folder=os.path.join(config.model_folder, "fixed"),
+        model_folder=os.path.join(config.model_folder,f"fixed_testing_{use_testing_mode}_itd_{input_t_dim}_ntsf{n_timesteps_forecasting}_ti{timeseries_interval}_klein_{klein}"),
         data_csv_path=config.data_csv_path,
         data_formatter=formatter,
-        use_testing_mode=True)  # Change to false to use original default params
+        use_testing_mode=use_testing_mode,
+        )  # Change to false to use original default params
     # %%
+#python script_train_fixed_params.py -expt_name kidfail -output_folder /app/tft_outputs/ -use_gpu yes -use_testing_mode no -klein yes -num_encoder_steps 56 -n_timesteps_forecasting 20 -timeseries_interval 6 -input_t_dim 120 -num_epochs 1
