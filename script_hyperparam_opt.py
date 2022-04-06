f84d66bb4bfb25be41483038950d5e9a4169a30d
Apsylem <Alexander.Vocaet@gmail.com>
2022-04-06 10:04:19 +0200

    rdy for kidfail

--- script_hyperparam_opt.py
+++ script_hyperparam_opt.py
@@ -52,7 +52,7 @@ def main(expt_name, use_gpu, restart_opt, model_folder, hyperparam_iterations,
     data_formatter: Dataset-specific data fromatter (see
       expt_settings.dataformatter.GenericDataFormatter)
   """
-
+  
   if not isinstance(data_formatter, data_formatters.base.GenericDataFormatter):
     raise ValueError(
         "Data formatters should inherit from" +
@@ -68,8 +68,14 @@ def main(expt_name, use_gpu, restart_opt, model_folder, hyperparam_iterations,
 
   print("### Running hyperparameter optimization for {} ###".format(expt_name))
   print("Loading & splitting data...")
-  raw_data = pd.read_csv(data_csv_path, index_col=0)
-  train, valid, test = data_formatter.split_data(raw_data)
+  if not data_formatter.pre_split_data_available:
+    raw_data = pd.read_csv(data_csv_path, index_col=0)
+    train, valid, test = data_formatter.split_data(raw_data)
+    
+  if data_formatter.pre_split_data_available:
+    train, valid, test = data_formatter.load_splitted_data()
+    
+    
   train_samples, valid_samples = data_formatter.get_num_samples_for_calibration(
   )
 
@@ -82,6 +88,7 @@ def main(expt_name, use_gpu, restart_opt, model_folder, hyperparam_iterations,
   opt_manager = HyperparamOptManager(param_ranges, fixed_params, model_folder)
 
   success = opt_manager.load_results()
+ 
   if success and not restart_opt:
     print("Loaded results from previous training")
   else:
@@ -99,6 +106,7 @@ def main(expt_name, use_gpu, restart_opt, model_folder, hyperparam_iterations,
       tf.compat.v1.keras.backend.set_session(sess)
 
       params = opt_manager.get_next_parameters()
+      
       model = ModelClass(params, use_cudnn=use_gpu)
 
       if not model.training_data_cached():
@@ -163,6 +171,8 @@ def main(expt_name, use_gpu, restart_opt, model_folder, hyperparam_iterations,
   print()
   print("Normalised Quantile Loss for Test Data: P50={}, P90={}".format(
       p50_loss.mean(), p90_loss.mean()))
+  import pickle
+  pickle.dump(best_params,open(os.path.join(opt_manager.hyperparam_folder, "best_params_pickle.pkl"),'wb'))
 
 
 if __name__ == "__main__":
@@ -174,58 +184,89 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description="Data download configs")
     parser.add_argument(
-        "expt_name",
-        metavar="e",
+        "-expt_name",  
         type=str,
         nargs="?",
         default="volatility",
         choices=experiment_names,
         help="Experiment Name. Default={}".format(",".join(experiment_names)))
     parser.add_argument(
-        "output_folder",
-        metavar="f",
+        "-output_folder",  
         type=str,
         nargs="?",
         default=".",
         help="Path to folder for data download")
     parser.add_argument(
-        "use_gpu",
-        metavar="g",
+        "-use_gpu",  
         type=str,
         nargs="?",
         choices=["yes", "no"],
         default="no",
         help="Whether to use gpu for training.")
     parser.add_argument(
-        "restart_hyperparam_opt",
-        metavar="o",
+        "-restart_hyperparam_opt",  
         type=str,
         nargs="?",
         choices=["yes", "no"],
         default="yes",
         help="Whether to re-run hyperparameter optimisation from scratch.")
-
+    parser.add_argument(
+            "-klein",      
+            type=str,
+            nargs="?",
+            choices=["yes", "no"],
+            default="no",
+            help="wether to use klein Dataset")
+    parser.add_argument(
+            "-num_encoder_steps",      
+            type=int,
+            nargs='?',
+            default=56,
+            help="how many intervalls should be given es as input")
+    parser.add_argument(
+            "-n_timesteps_forecasting",      
+            type=int,
+            nargs='?',
+            default=20,
+            help="how many intervalls should be forecasted")
+    parser.add_argument(
+            "-timeseries_interval",      
+            type=int,
+            nargs='?',
+            default=6,
+            help="how long one timeseries intervall should be")
+    parser.add_argument(
+            "-input_t_dim",      
+            type=int,
+            nargs='?',
+            default=120,
+            help="how long one timeseries intervall should be")
+#-expt_name kidfail -output_folder /app/tft_outputs/ -use_gpu yes -restart_hyperparam_opt no -klein yes -num_encoder_steps 56 -n_timesteps_forecasting 20 -timeseries_interval 5 -input_t_dim 120
     args = parser.parse_known_args()[0]
 
     root_folder = None if args.output_folder == "." else args.output_folder
-
-    return args.expt_name, root_folder, args.use_gpu == "yes", \
-        args.restart_hyperparam_opt == "yes"
+  
+    return args.expt_name, root_folder, args.use_gpu == 'yes', \
+        args.restart_hyperparam_opt == 'yes', args.klein == 'yes',\
+        args.num_encoder_steps,args.n_timesteps_forecasting,args.timeseries_interval,args.input_t_dim,
 
   # Load settings for default experiments
-  name, folder, use_tensorflow_with_gpu, restart = get_args()
+  name, folder, use_tensorflow_with_gpu, restart, klein, num_encoder_steps,n_timesteps_forecasting,timeseries_interval,input_t_dim = get_args()
 
-  print("Using output folder {}".format(folder))
 
   config = ExperimentConfig(name, folder)
   formatter = config.make_data_formatter()
-
+  formatter = formatter(klein=klein,
+                        num_encoder_steps = num_encoder_steps ,
+                        n_timesteps_forecasting = n_timesteps_forecasting,
+                        timeseries_interval = timeseries_interval,
+                        input_t_dim = input_t_dim)
   # Customise inputs to main() for new datasets.
   main(
       expt_name=name,
       use_gpu=use_tensorflow_with_gpu,
       restart_opt=restart,
-      model_folder=os.path.join(config.model_folder, "main"),
+      model_folder=os.path.join(config.model_folder, f"main_itd_{input_t_dim}_ntsf{n_timesteps_forecasting}_ti{timeseries_interval}_klein_{klein}"),
       hyperparam_iterations=config.hyperparam_iterations,
       data_csv_path=config.data_csv_path,
       data_formatter=formatter)
